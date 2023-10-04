bdc20c862695f8d54bf04c93ad5c76f044dcafea
Krystian Duma <git@krystian.duma.sh>
2023-10-04 21:25:39 +0200

    Update release script

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -6,7 +6,7 @@ dir=$( dirname -- "$( readlink -f -- "$0"; )"; )
 
 if [ -z "$(cd "$dir/../" && git status --porcelain)" ]
 then
-  version=$(cd "$dir/../.scripts/" && ./bump_version.sh 2 VERSION) && echo "Releasing version $version" && \
+  version=$(cd "$dir/../.scripts/" && ./bump_version.sh ${1:-2} VERSION) && echo "Releasing version $version" && \
   cd "$dir/../example/" && npm install && npm run build:android && \
   cd "$dir/../" && npm install && npm run verify && npm run build && \
   cd "$dir/../" && git add --all && git commit -m "Release $version" && git tag -a "v$version" -m "Release $version" && git push && git push --tags
